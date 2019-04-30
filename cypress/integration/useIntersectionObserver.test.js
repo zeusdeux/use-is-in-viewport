63c10348f112381a1f1f3b86a2108bd882196b94
Mudit Ameta <zeusdeux@gmail.com>
2019-04-30 05:03:16 +0200

    Start updating integration tests

--- cypress/integration/useIntersectionObserver.test.js
+++ cypress/integration/useIntersectionObserver.test.js
@@ -1,30 +1,45 @@
+before(() => {
+  cy.visit('/')
+})
+
 describe('useIsInViewport', () => {
-  beforeEach(() => {
-    cy.visit('/')
-    cy.scrollTo(0, 0)
-  })
-
-  it('should work for element with a custom viewport', () => {
-    cy.get('[data-testid="div-viewport-parent-el"]').should(
+  it('should report correctly when an element is visible in parent document', () => {
+    cy.get('[data-testid="toggle-simple-parent-doc-test"]').should(
       'contain',
-      'Not intersecting with parent'
+      'Hide simple parent doc test'
     )
-    cy.get('[data-testid="toggle-container-size"]').click()
-    cy.get('[data-testid="div-viewport-parent-el"]').should('contain', 'Intersecting with parent')
-    cy.get('[data-testid="toggle-container-size"]').click()
-    cy.get('[data-testid="div-viewport-parent-el"]').should(
-      'contain',
-      'Not intersecting with parent'
-    )
+    cy.get('[data-testid="box"]').should('contain', 'In viewport')
+    cy.get('[data-testid="toggle-box-position"]').click()
+    cy.get('[data-testid="box"]').should('contain', 'Out of viewport')
+    cy.scrollTo(0, 430)
+    cy.get('[data-testid="box"]').should('contain', 'In viewport')
+    cy.scrollTo(0, 600)
+    cy.get('[data-testid="box"]').should('contain', 'In viewport')
   })
 
-  it('should work for elements that intersect with their parent document', () => {
-    cy.get('[data-testid="div-viewport-window"]')
-      .should('contain', 'Hidden')
-      .scrollIntoView()
-      .should('contain', 'Visible')
+  // it('should report correctly when ')
 
-    cy.scrollTo(0, 0)
-    cy.get('[data-testid="div-viewport-window"]').should('contain', 'Hidden')
-  })
+  // it('should work for element with a custom viewport', () => {
+  //   cy.get('[data-testid="div-viewport-parent-el"]').should(
+  //     'contain',
+  //     'Not intersecting with parent'
+  //   )
+  //   cy.get('[data-testid="toggle-container-size"]').click()
+  //   cy.get('[data-testid="div-viewport-parent-el"]').should('contain', 'Intersecting with parent')
+  //   cy.get('[data-testid="toggle-container-size"]').click()
+  //   cy.get('[data-testid="div-viewport-parent-el"]').should(
+  //     'contain',
+  //     'Not intersecting with parent'
+  //   )
+  // })
+
+  // it('should work for elements that intersect with their parent document', () => {
+  //   cy.get('[data-testid="div-viewport-window"]')
+  //     .should('contain', 'Hidden')
+  //     .scrollIntoView()
+  //     .should('contain', 'Visible')
+
+  //   cy.scrollTo(0, 0)
+  //   cy.get('[data-testid="div-viewport-window"]').should('contain', 'Hidden')
+  // })
 })
